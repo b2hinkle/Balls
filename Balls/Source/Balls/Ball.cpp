296a2d43b45599f4c4809bfc13682bf96fbc7176
b2hinkle <b2hinkle@gmail.com>
2023-11-11 05:04:22 -0500

    Hook up overlap events correctly

--- Balls/Source/Balls/Ball.cpp
+++ Balls/Source/Balls/Ball.cpp
@@ -11,6 +11,7 @@ ABall::ABall()
     RootComponent = RootMeshComponent;
 
     RootMeshComponent->BodyInstance.bSimulatePhysics = true;
+    RootMeshComponent->BodyInstance.bNotifyRigidBodyCollision = true;
 }
 
 void ABall::BeginPlay()
@@ -28,24 +29,24 @@ void ABall::Tick(float DeltaTime)
 
 void ABall::OnHitCallback(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-    //FActorSpawnParameters spawnParams;
-    //spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+    FActorSpawnParameters spawnParams;
+    spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
 
-    //const float dividedAngle = (2 * UE_PI) / SpawnCount;
+    const float dividedAngle = (2 * UE_PI) / SpawnCount;
 
-    //const FVector& circleUpVector = Hit.ImpactNormal;
-    //const FVector& circleLocation = Hit.ImpactPoint;
+    const FVector& circleUpVector = Hit.ImpactNormal;
+    const FVector& circleLocation = Hit.ImpactPoint;
 
-    //const FVector& circleForwardVector = FRotationMatrix::MakeFromZ(circleUpVector).GetUnitAxis(EAxis::X);
+    const FVector& circleForwardVector = FRotationMatrix::MakeFromZ(circleUpVector).GetUnitAxis(EAxis::X);
 
-    //for (uint8 i = 0; i < SpawnCount; i++)
-    //{
-    //    const float rotationAngle = dividedAngle * i;
+    for (uint8 i = 0; i < SpawnCount; i++)
+    {
+        const float rotationAngle = dividedAngle * i;
 
-    //    const FVector& indexForwardVector = circleForwardVector.RotateAngleAxisRad(rotationAngle, circleUpVector);
+        const FVector& indexForwardVector = circleForwardVector.RotateAngleAxisRad(rotationAngle, circleUpVector);
 
-    //    const FVector& indexSpawnLocation = circleLocation + (indexForwardVector * SpawnCircleRadius);
+        const FVector& indexSpawnLocation = circleLocation + (indexForwardVector * SpawnCircleRadius);
 
-    //    GetWorld()->SpawnActor<AActor>(ActorClassToSpawnOnImpact, indexSpawnLocation, FRotator::ZeroRotator, spawnParams);
-    //}
+        GetWorld()->SpawnActor<AActor>(ActorClassToSpawnOnImpact, indexSpawnLocation, FRotator::ZeroRotator, spawnParams);
+    }
 }
